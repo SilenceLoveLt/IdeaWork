f1334da0ac6b0c5c903995e41df5eb26d8efd6ba
yangyukun@lt.com <yangyukun@lt.com>
2019-08-18 21:02:58 +0800

    commit

--- src/main/java/com/yyk/Application.java
+++ src/main/java/com/yyk/Application.java
@@ -3,13 +3,14 @@ package com.yyk;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * 此处加上@EnableSwagger2注解 才能使用swagger
  * 加上@EnableCaching 使用redis
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @MapperScan({ "com.yyk.dao"})
 @EnableCaching
 public class Application {
